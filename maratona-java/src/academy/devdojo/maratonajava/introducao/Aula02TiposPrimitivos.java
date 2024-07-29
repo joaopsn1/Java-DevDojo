49ce441230eb6ab4de5f1e6fffe59ceb6d490a45
João Pedro <joaopsn1@outlook.com.br>
2024-07-29 10:21:16 -0300

    .

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = (int) 10000000000L;
+        long numeroGrande = (long) 155.23;
+        double salarioDouble = 2000.00;
+        float salarioFloat = 2500.00F;
+        byte idadeByte = 127;
+        short idadeShort = 32000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 88;
+
+        System.out.println("A idade é: " + idade + " anos");
+        System.out.println(falso);
+        System.out.println("Char: " + caractere);
+        System.out.println(salarioFloat);
+        System.out.println(numeroGrande);
+    }
+}
