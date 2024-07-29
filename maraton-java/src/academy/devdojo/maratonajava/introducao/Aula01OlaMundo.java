2e29dbfad7a06d4a11e514cfcdeb12e8babc20bc
João Pedro <joaopsn1@outlook.com.br>
2024-07-29 10:07:35 -0300

    comeco curso java

---
+++ maraton-java/src/academy/devdojo/maratonajava/introducao/Aula01OlaMundo.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
+
+public class Aula01OlaMundo {
+    // Isso é um comentário de uma linha
+    /*
+        Isso é um comentário
+        de multiplas
+        linhas
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+}
