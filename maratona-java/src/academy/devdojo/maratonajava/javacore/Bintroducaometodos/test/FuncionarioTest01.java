6749792588390d1f1ad352742263ba5a626f51a4
João Pedro <joaopsn1@outlook.com.br>
2024-08-01 17:57:01 -0300

    .

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -6,10 +6,10 @@ public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
 
-        funcionario.nome = "João Pedro";
-        funcionario.idade = 22;
-        funcionario.salarios = new double[]{2500, 3000, 5000};
-
+        funcionario.setNome("João Pedro");
+        funcionario.setIdade(22);
+        funcionario.setSalarios(new double[]{2500, 3000, 5000});
         funcionario.imprime();
+        System.out.println("Média: " + funcionario.getMedia());
     }
 }
