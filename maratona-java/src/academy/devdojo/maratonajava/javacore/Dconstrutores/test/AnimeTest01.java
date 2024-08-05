6553ef95d5942953e772936863e15f894fbad8ff
João Pedro <joaopsn1@outlook.com.br>
2024-08-04 21:58:18 -0300

    Construtores

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Dconstrutores.test;
+
+import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("One Piece", "TV", 1056, "Piratas", "Toei");
+        anime.imprime();
+    }
+}
