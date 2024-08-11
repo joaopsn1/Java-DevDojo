bff74ae5deb2ffb594a3ec24eb03b367fbdc5634
João Pedro <joaopsn1@outlook.com.br>
2024-08-10 22:48:25 -0300

    exercicio de associação

---
+++ exercicios-poo/src/dominio/futebol/Clube.java
@@ -0,0 +1,59 @@
+package dominio.futebol;
+
+public class Clube {
+    private String nomeClube;
+    private Jogador[] jogadores;
+    private Estado estado;
+
+    public Clube(Estado estado) {
+        this.estado = estado;
+    }
+
+    public Clube(String nomeClube, Estado estado) {
+        this.nomeClube = nomeClube;
+        this.estado = estado;
+    }
+
+    public Clube(String nomeClube, Jogador[] jogadores, Estado estado) {
+        this.nomeClube = nomeClube;
+        this.jogadores = jogadores;
+        this.estado = estado;
+    }
+
+    public void imprime() {
+        System.out.println("Clube: " + this.nomeClube);
+        System.out.println("Estado: " + estado.getEstado());
+        if (jogadores == null) {
+            System.out.println("Clube sem Jogadores. Necessário Contratar!");
+            return;
+        }
+        System.out.println("Jogadores");
+        for (Jogador jogador : jogadores) {
+            System.out.println(jogador.getNome());
+        }
+    }
+
+    public String getNomeClube() {
+        return nomeClube;
+    }
+
+    public void setNomeClube(String nomeClube) {
+        this.nomeClube = nomeClube;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public Jogador[] getJogadores() {
+        return jogadores;
+    }
+
+    public void setJogadores(Jogador[] jogadores) {
+        this.jogadores = jogadores;
+    }
+}
