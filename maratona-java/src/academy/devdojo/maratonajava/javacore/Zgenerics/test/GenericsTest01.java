7507e8c5e01ccbba914b354dfa499a81e2d505d9
João Pedro <joaopsn1@outlook.com.br>
2024-10-23 10:59:00 -0300

    introdução a generics

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/GenericsTest01.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Consumidor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsTest01 {
+    public static void main(String[] args) {
+        // Type erasure
+        List<String> lista = new ArrayList<>();
+        lista.add("Midorya");
+        lista.add("Luffy");
+
+
+        for (String o : lista) {
+            System.out.println(o);
+        }
+
+        add(lista, new Consumidor("Midorya"));
+    }
+
+    public static void add(List lista, Consumidor consumidor) {
+        lista.add(consumidor);
+    }
+}
