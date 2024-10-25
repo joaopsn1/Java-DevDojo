f186c264ea1be071e33759380dd757b1e160cee0
João Pedro <joaopsn1@outlook.com.br>
2024-10-25 19:28:42 -0300

    classes internas

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZAclassesinternas/test/OuterClassesTest02.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.ZZAclassesinternas.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.exercicio.dominio.Local;
+
+public class OuterClassesTest02 {
+    private String name = "Midorya";
+
+    void print(final String param) { //também deve ser efetivamente final
+        final String lastName = "Izuku"; //precisa ser final
+        class LocalClass {
+            public void printLocal() {
+                System.out.println(param);
+                System.out.println(name + " " + lastName);
+            }
+        }
+        new LocalClass().printLocal();
+    }
+
+    public static void main(String[] args) {
+        OuterClassesTest02 outer = new OuterClassesTest02();
+        outer.print();
+    }
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/MetodoGenericoTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Barco;
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Carro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MetodoGenericoTest01 {
+    public static void main(String[] args) {
+        List<Barco> barcoList = criarArrarComUmObjeto(new Barco("Canoa Marota"));
+        System.out.println(barcoList);
+    }
+
+    private static <T> List<T> criarArrarComUmObjeto(T t) {
+        return List.of(t);
+    }
+
+//    private static <T extends Comparable<T>> List<T> criarArrarComUmObjeto(T t) {
+//        return List.of(t);
+//    }
+}
+
