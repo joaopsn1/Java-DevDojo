8c5927f21bf796e084f0c31b9980b412375c524e
João Pedro <joaopsn1@outlook.com.br>
2024-10-28 22:19:38 -0300

    lambdas e method reference

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest02.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import academy.devdojo.maratonajava.javacore.ZZClambdas.dominio.Anime;
+import academy.devdojo.maratonajava.javacore.ZZClambdas.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Reference to an instance method of a particular object
+public class MethodReferenceTest02 {
+    public static void main(String[] args) {
+        AnimeComparators animeComparators = new AnimeComparators();
+        List<Anime> animeList = new ArrayList<>(List.of(new Anime("Berserk", 43), new Anime("One Piece", 1100), new Anime("Naruto", 700)));
+        animeList.sort(animeComparators::compareByEpisodesNonStatic);
+        System.out.println(animeList);
+    }
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest04.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import academy.devdojo.maratonajava.javacore.ZZClambdas.dominio.Anime;
+import academy.devdojo.maratonajava.javacore.ZZClambdas.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+//Reference to a constructor
+public class MethodReferenceTest04 {
+    public static void main(String[] args) {
+        Supplier<AnimeComparators> newAnimeComparators = AnimeComparators::new;
+        AnimeComparators animeComparators = newAnimeComparators.get();
+        List<Anime> animeList = new ArrayList<>(List.of(new Anime("Berserk", 43), new Anime("One Piece", 1100), new Anime("Naruto", 700)));
+        animeList.sort(animeComparators::compareByEpisodesNonStatic);
+        System.out.println(animeList);
+
+        BiFunction<String, Integer, Anime> animeBiFunction = (title, episodes) -> new Anime(title, episodes);
+        BiFunction<String, Integer, Anime> animeBiFunction2 = Anime::new;
+        System.out.println(animeBiFunction2.apply("Super Campeões", 36));
+    }
+}
