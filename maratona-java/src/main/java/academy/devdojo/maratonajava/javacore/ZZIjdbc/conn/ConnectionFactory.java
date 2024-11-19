6e154bba1dc44d797ff9af06818d79136e12892d
João Pedro <joaopsn1@outlook.com.br>
2024-11-18 22:14:21 -0300

    docker, mysql e maven - Aula JDBC

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/conn/ConnectionFactory.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.ZZIjdbc.conn;
+
+import java.sql.Connection;
+
+public class ConnectionFactory {
+    //java.sql = Connection, Statement, ResultSet, DriverManager
+    public static void main(String[] args) {
+        
+    }
+}
