0fdad3846ab912595f9e7751e8298a3a24f8365c
João Pedro <joaopsn1@outlook.com.br>
2024-11-21 15:26:06 -0300

    mais conteudo de jdbc

--- maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
@@ -4,9 +4,9 @@ import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import lombok.extern.log4j.Log4j2;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Log4j2
 public class ProducerRepository {
@@ -15,9 +15,105 @@ public class ProducerRepository {
         try (Connection conn = ConnectionFactory.getConnection();
              Statement stmt = conn.createStatement()) {
             int rowsAffected = stmt.executeUpdate(sql);
-            log.info("Inserted producer in the database, rows affected '{}'", rowsAffected);
+            log.info("Inserted producer '{}' in the database, rows affected '{}'", producer.getName(), rowsAffected);
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.error("Error while trying to insert producer '{}'", producer.getName(), e);
+        }
+    }
+
+    public static void delete(int id) {
+        String sql = "DELETE FROM `anime_store`.`producer` WHERE (`id` = '%d');".formatted(id);
+        try (Connection conn = ConnectionFactory.getConnection();
+             Statement stmt = conn.createStatement()) {
+            int rowsAffected = stmt.executeUpdate(sql);
+            log.info("Deleted producer '{}' in the database, rows affected '{}'", id, rowsAffected);
+        } catch (SQLException e) {
+            log.error("Error while trying to delete producer '{}'", id, e);
+        }
+    }
+
+    public static void update(Producer producer) {
+        String sql = "UPDATE `anime_store`.`producer` SET `name` = '%s' WHERE (`id` = '%d');".formatted(producer.getName(), producer.getId());
+        try (Connection conn = ConnectionFactory.getConnection();
+             Statement stmt = conn.createStatement()) {
+            int rowsAffected = stmt.executeUpdate(sql);
+            log.info("Updated producer '{}' in the database, rows affected '{}'", producer.getId(), rowsAffected);
+        } catch (SQLException e) {
+            log.error("Error while trying to update producer '{}'", producer.getId(), e);
+        }
+    }
+
+    public static List<Producer> findAll() {
+        log.info("Finding all Producers");
+        return findByName("");
+    }
+
+    public static List<Producer> findByName(String name) {
+        log.info("Finding Producers by Name");
+        String sql = "SELECT * FROM anime_store.producer where name like '%%%s%%';"
+                .formatted(name);
+        List<Producer> producers = new ArrayList<>();
+        try (Connection conn = ConnectionFactory.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                Producer producer = Producer
+                        .builder()
+                        .id(rs.getInt("id"))
+                        .name(rs.getString("name"))
+                        .build();
+                producers.add(producer);
+            }
+        } catch (SQLException e) {
+            log.error("Error while trying to find all producers", e);
+        }
+        return producers;
+    }
+
+    public static void showProducerMetaData() {
+        log.info("Showing Producer MetaData");
+        String sql = "SELECT * FROM anime_store.producer";
+        try (Connection conn = ConnectionFactory.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            ResultSetMetaData rsMetaData = rs.getMetaData(); // Pega os metadados
+            int columnCount = rsMetaData.getColumnCount();// Quantidade de colunas que tenho na minha tabela
+            log.info("Columns count '{}'", columnCount);
+            for (int i = 1; i <= columnCount; i++) {
+                log.info("Table name '{}'", rsMetaData.getTableName(i));
+                log.info("Column name '{}'", rsMetaData.getColumnName(i));
+                log.info("Column size '{}'", rsMetaData.getColumnDisplaySize(i));
+                log.info("Column type '{}'", rsMetaData.getColumnTypeName(i));
+            }
+        } catch (SQLException e) {
+            log.error("Error while trying to find all producers", e);
+        }
+    }
+
+    public static void showDriverMetaData() {
+        log.info("Showing Driver MetaData");
+        try (Connection conn = ConnectionFactory.getConnection()) {
+           DatabaseMetaData dbMetaData = conn.getMetaData();
+           if (dbMetaData.supportsResultSetType(ResultSet.TYPE_FORWARD_ONLY)) { // ResultSet.TYPE_FORWARD_ONLY Cursor só pode avançar;
+               log.info("Supports TYPE_FORWARD_ONLY");
+               if (dbMetaData.supportsResultSetConcurrency(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE)) { // ResultSet.CONCUR_UPDATABLE objeto que pode ser atualizado
+                   log.info("And Supports CONCUR_UPDATABLE");
+               }
+           }
+            if (dbMetaData.supportsResultSetType(ResultSet.TYPE_SCROLL_INSENSITIVE)) { // ResultSet.TYPE_SCROLL_INSENSITIVE Cursor pode ser movido para qualquer posição
+                log.info("Supports TYPE_SCROLL_INSENSITIVE");
+                if (dbMetaData.supportsResultSetConcurrency(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
+                    log.info("And Supports CONCUR_UPDATABLE");
+                }
+            }
+            if (dbMetaData.supportsResultSetType(ResultSet.TYPE_SCROLL_SENSITIVE)) { // ResultSet.TYPE_SCROLL_SENSITIVE Cursor pode ser movido para frente ou para trás
+                log.info("Supports TYPE_SCROLL_SENSITIVE");
+                if (dbMetaData.supportsResultSetConcurrency(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
+                    log.info("And Supports CONCUR_UPDATABLE");
+                }
+            }
+        } catch (SQLException e) {
+            log.error("Error while trying to find all producers", e);
         }
     }
 }
--- maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -2,12 +2,23 @@ package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.service.ProducerService;
 import lombok.extern.log4j.Log4j2;
+
+import java.util.List;
 
 @Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-        Producer producer = Producer.builder().name("Studio Deen").build();
-        ProducerRepository.save(producer);
+        Producer producer = Producer.builder().name("NHK").build();
+        Producer producerToUpdate = Producer.builder().id(3).name("Studio Deen").build();
+//        ProducerService.save(producer);
+//        ProducerService.delete(4);
+//        ProducerService.update(producerToUpdate);
+//        List<Producer> producers = ProducerService.findAll();
+//        List<Producer> producers = ProducerService.findByName("Mad");
+//        log.info("Producers found '{}'", producers);
+//        ProducerService.showProducerMetaData();
+        ProducerService.showDriverMetaData();
     }
 }
