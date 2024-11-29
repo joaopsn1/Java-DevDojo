0b907ed63017893eba905eeac7e8c02d97895ae0
João Pedro <joaopsn1@outlook.com.br>
2024-11-29 20:19:15 -0300

    testes unitarios

---
+++ maratona-java/src/test/java/academy/devdojo/maratonajava/javacore/ZZKjunit/service/PersonServiceTest.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.ZZKjunit.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonServiceTest {
+
+    @Test
+    void isAdult() {
+
+    }
+}
